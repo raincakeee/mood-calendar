4b001d282c7f005c3303e7c5a07dad417c5a4020
raincakeee <raincakeee@gmail.com>
2026-01-02 01:40:37 +0800

    Create App.tsx
    G

---
+++ mood-calendar-card (1)/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Mood Calendar</h1>
+      <button onClick={() => alert('clicked!')}>
+        点我试试
+      </button>
+    </div>
+  )
+}
+
+export default App
